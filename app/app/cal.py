6247d151439d34b576638d0510618d332f7b3746
mohamed_sakr <mohamed1.sakr1996@gmail.com>
2021-04-02 18:06:59 +0200

    add more test using TDD

--- app/app/cal.py
+++ app/app/cal.py
@@ -3,3 +3,7 @@
 def add(x, y):
     """ sum two intger numbers"""
     return x + y
+
+def subtract(x, y):
+    """ subtract x from y and return result"""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 
-from app.cal import add
+from app.cal import add, subtract
 
 class CalTest(TestCase):
 
     def test_add_numbers(self):
         """ test that test adding two numbers together """
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """ test that test subtracting two numbers and return value"""
+        self.assertEqual(subtract(8,5), 3)
