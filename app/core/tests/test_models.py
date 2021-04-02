069fd8e2332bd6bb6a995472689173d463292e57
mohamed_sakr <mohamed1.sakr1996@gmail.com>
2021-04-02 19:34:53 +0200

    normalize email for new user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test new user email is normalized"""
+        email = "mohamed@MAIL.COM"
+        user = get_user_model().objects.create_user(email, '123456789')
+
+        self.assertEqual(user.email, email.lower())
