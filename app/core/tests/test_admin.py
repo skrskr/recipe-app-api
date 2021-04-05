6f6956de7fb6bd4e483a751641cf689f4a4ca3f6
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2021-04-05 12:02:32 +0200

    customize admin dashboard with tests to support custom  user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,41 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSitesTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_super_user(
+            'superadmin@admin.com',
+            'password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'admin@admin.com',
+            password = 'password123',
+            name="Tes Admin"
+        )
+
+    def test_users_list(self):
+        """test that users are listed on user page """
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """ test that change user page work fine """
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_create_page(self):
+        """ test that create user page work fine """
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
